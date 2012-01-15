2b4e0539fdec90cc4cfcc8fd8683be27aea544b3
Antonio El Khoury <aelkhour@laas.fr>
2012-01-15 23:02:21 +0100

    Predeclare classes in fwd.hh

---
+++ include/roboptim/capsule/fwd.hh
@@ -0,0 +1,37 @@
+// Copyright (C) 2011 by Antonio El Khoury.
+//
+// This file is part of the roboptim-capsule.
+//
+// roboptim-capsule is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// roboptim-capsule is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with roboptim-capsule.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * \brief Forward declarations.
+ */
+
+#ifndef KCD_ROBOPTIM_FWD_HH
+# define KCD_ROBOPTIM_FWD_HH
+
+namespace roboptim
+{
+  namespace capsule
+  {
+    class Volume;
+    class DistanceCapsulePoint;
+    class DistanceCapsulePolyhedron;
+    class DistanceSegmentPolyhedron;
+    class Fitter;
+  } // end of namespace capsule.
+} // end of namespace kcd.
+
+#endif //! KCD_ROBOPTIM_FWD_HH
